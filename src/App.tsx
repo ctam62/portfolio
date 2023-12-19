2df66c1e068b24ba97b97e7b146546c1aaf633e5
claratam <claramtam@gmail.com>
2023-12-19 13:25:45 -0600

    feat: install router module; import site components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header/Header';
+import { Hero } from './components/Hero/Hero';
+import { About } from './components/About/About';
+import { Skills } from './components/Skills/Skills';
+import { Projects } from './components/Projects/Projects';
+import { Footer } from './components/Footer/Footer';
+
 
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Hero />
+                <About />
+                <Skills />
+                <Projects />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    )
 }
 
-export default App
+export default App;
