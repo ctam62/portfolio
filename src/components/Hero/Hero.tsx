04464e6ab762509377977ee2c22b0efc0bc8a973
claratam <claramtam@gmail.com>
2023-12-20 22:33:31 -0600

    feat: add hero content and styles

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,26 @@
 import './Hero.scss';
+import portrait from '../../assets/images/portrait.png';
 
 
 export const Hero = () => {
     return (
         <section className="hero" id="hero">
-            
+            <div className="hero__content">
+                <article className="hero__content-col hero__content-col--img"></article>
+                <article className="hero__content-col hero__content-col--mid">
+                    <span className="circle"></span>
+                    <span className="line"></span>
+                    <span className="circle--bottom"></span>
+                </article>
+                <article className="hero__content-col">
+                    <h1 className="hero__heading">Clara Tam</h1>
+                    <p className="hero__text">
+                        A software engineer passionate in creating meaningful
+                        zuser-friendly applications balancing aesthetics and functionality.
+                    </p>
+                    <button className="hero__button">VIEW PROJECTS</button>
+                </article>
+            </div>
         </section>
     )
 };
