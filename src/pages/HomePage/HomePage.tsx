43083dc05f84d76ae972ad20d59de23cbb2ae0a7
claratam <claramtam@gmail.com>
2023-12-21 03:00:52 -0600

    feat: import contact component to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,6 +3,7 @@ import { Hero } from '../../components/Hero/Hero';
 import { About } from '../../components/About/About';
 import { Skills } from '../../components/Skills/Skills';
 import { Projects } from '../../components/Projects/Projects';
+import { Contact } from '../../components/Contact/Contact';
 
 
 export const HomePage = () => {
@@ -12,6 +13,7 @@ export const HomePage = () => {
             <About />            
             <Skills />
             <Projects />
+            <Contact />
         </main>
     )
 };
