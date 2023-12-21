0e9a1711096696f72e965531b4707dc4ceb091e0
claratam <claramtam@gmail.com>
2023-12-21 00:11:19 -0600

    feat: add about section content

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,10 +1,35 @@
 import './About.scss';
+import signature from '../../assets/images/personal_signature.png';
 
 
 export const About = () => {
     return (
         <section className="about" id="about">
-            <h2>About Me</h2>
+            <article className="about__content">
+                <span className="about__line--primary"></span>
+                <span className="about__circle--primary"></span>
+
+                <h2 className="about__heading">About Me</h2>
+
+                <p className="about__text">
+                    I am a software-engineer passionate about problem-solving and leveraging technology to build innovative solutions.
+                </p>
+                <p className="about__text">
+                    My expertise spans media art design and web development, with a strong foundation in biomedical engineering, machine learning, and natural language processing. My diverse skill set allows me to excel in team collaboration, where I contribute not only technical proficiency but also the ability to articulate complex concepts to non-technical stakeholders.
+                </p>
+                <p className="about__text">
+                    I thrive on creating meaningful applications that merge cutting-edge technology with creative problem-solving, embodying a commitment to both functionality and aesthetic excellence.
+                </p>
+
+                <img
+                    className="about__signature"
+                    src={signature}
+                    alt="claratam"
+                />
+
+                <span className="about__circle--primary"></span>
+                <span className="about__line--primary"></span>
+            </article>
         </section>
     )
 };
