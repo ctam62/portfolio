6d475c5a3b231ab4f5ba5c14d1f0c13b5dda9615
claratam <claramtam@gmail.com>
2023-12-21 02:59:14 -0600

    feat: update hero styles for mobile, moved line graphic position; update header nav background styles and position; update about line styles; update app site padding

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,17 +6,17 @@ export const Hero = () => {
         <section className="hero" id="hero">
             <div className="hero__content">
                 <article className="hero__content-col hero__content-col--img"></article>
-                <article className="hero__content-col hero__content-col--mid">
-                    <span className="hero__circle"></span>
-                    <span className="hero__line"></span>
-                </article>
-                <article className="hero__content-col">
+                <article className="hero__content-col hero__content-col--right">
                     <h1 className="hero__heading">Clara Tam</h1>
                     <p className="hero__text">
                         A software engineer passionate in creating meaningful
                         user-friendly applications balancing aesthetics and functionality.
                     </p>
                     <button className="hero__button">VIEW PROJECTS</button>
+                </article>
+                <article className="hero__content-col hero__content-col--mid">
+                    <span className="hero__circle"></span>
+                    <span className="hero__line"></span>
                 </article>
             </div>
         </section>
