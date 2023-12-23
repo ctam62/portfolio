2f7125cc3352ee078f782c6b8266e9f6d44828bd
claratam <claramtam@gmail.com>
2023-12-23 15:38:49 -0600

    feat: update about section text; update header nav link paths

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -12,13 +12,13 @@ export const About = () => {
 
                 <div className="about__text-block">
                     <p className="about__text">
-                        I am a software engineer passionate about problem-solving and leveraging technology to build innovative solutions.
+                        Hello, my name is Clara. I enjoy problem-solving and leveraging technology to build innovative web solutions.
                     </p>
                     <p className="about__text">
                         My expertise spans media art design and web development, with a strong foundation in biomedical engineering, machine learning, and natural language processing. My diverse skill set allows me to excel in team collaboration, where I contribute not only technical proficiency but also the ability to articulate complex concepts to non-technical stakeholders.
                     </p>
                     <p className="about__text">
-                        I thrive on creating meaningful applications that merge cutting-edge technology with creative problem-solving, embodying a commitment to both functionality and aesthetic excellence.
+                        Currently, I've been working with technologies such as TypeSript, Vercel, Netlify, Django, and AWS.
                     </p>
                 </div>
 
