6b8e47a4cd1f09b9e60b58d5b2cdcbdedcb2f21c
claratam <claramtam@gmail.com>
2023-12-23 10:53:27 -0600

    feat: update project page image assets; add project image content; move hide class to global stylesheet; hide empty content

--- src/pages/ProjectPage/ProjectPage.tsx
+++ src/pages/ProjectPage/ProjectPage.tsx
@@ -1,58 +1,70 @@
 import './ProjectPage.scss';
+import { useState } from 'react';
 import { useParams } from 'react-router';
 import data from '../../data/projects.json';
 
 
 export const ProjectPage = () => {
+
+    const [visible, setVisible] = useState(false);
+
     const { title } = useParams();
     const project = data.filter(item => item.title.toLowerCase() === title)[0];
+    const filename = title?.toLowerCase().replace(/\s/g, "");
 
     return (
         <main className="project">
             <section className="project__content">
-                <article className="project__description">
-                    <h2 className="project__title">{project.title}</h2>
-                    <p className="project__summary">{project.description}</p>
-
-                    <div className="project__details">
-                        <p className="project__text">
-                            <span className="project__subheading">Client: </span>
-                            {project.client}
-                        </p>
-
-                        <p className="project__text">
-                            <span className="project__subheading">Website: </span>
-                            {project.url}
-                        </p>
-
-                        <p className="project__text">
-                            <span className="project__subheading">Date: </span>
-                            {project.date}
-                        </p>
-
-                        <p className="project__text">
-                            <span className="project__subheading">Brief: </span>
-                            {project.brief}
-                        </p>
-
-                        <p className="project__text">
-                            <span className="project__subheading">Concept: </span>
-                            {project.concept}
-                        </p>
-
-                        <p className="project__text">
-                            <span className="project__subheading">Deliverables: </span>
-                            {project.deliverables}
-                        </p>
-
-                        <p className="project__text">
-                            <span className="project__subheading">Demographic: </span>
-                            {project.demographic}
-                        </p>
+                <div className="project__content-col">
+                    <div
+                        className="project__imgs"
+                        style={{ background: `center/cover no-repeat url(/src/assets/images/${filename}.jpg)` }}
+                    >
                     </div>
-                </article>
+                </div>
+                <div className="project__content-col">
+                    <article className="project__description">
+                        <h2 className="project__title">{project.title}</h2>
+                        <p className="project__summary">{project.description}</p>
 
-                <img className="project__imgs" src='' alt={`${title?.toLowerCase()} mockups`} />
+                        <div className="project__details">
+                            <p className={`project__text ${project.client === "" ? "hide" : ""}`}>
+                                <span className="project__subheading">Client: </span>
+                                {project.client}
+                            </p>
+
+                            <p className={`project__text ${project.url === "" ? "hide" : ""}`}>
+                                <span className="project__subheading">Website: </span>
+                                {project.url}
+                            </p>
+
+                            <p className={`project__text ${project.date === "" ? "hide" : ""}`}>
+                                <span className="project__subheading">Date: </span>
+                                {project.date}
+                            </p>
+
+                            <p className={`project__text ${project.brief === "" ? "hide" : ""}`}>
+                                <span className="project__subheading">Brief: </span>
+                                {project.brief}
+                            </p>
+
+                            <p className={`project__text ${project.concept === "" ? "hide" : ""}`}>
+                                <span className="project__subheading">Concept: </span>
+                                {project.concept}
+                            </p>
+
+                            <p className={`project__text ${project.deliverables === "" ? "hide" : ""}`}>
+                                <span className="project__subheading">Deliverables: </span>
+                                {project.deliverables}
+                            </p>
+
+                            <p className={`project__text ${project.demographic === "" ? "hide" : ""}`}>
+                                <span className="project__subheading">Demographic: </span>
+                                {project.demographic}
+                            </p>
+                        </div>
+                    </article>
+                </div>
             </section>
             <nav className="project__nav">
                 <ul className="project__nav-list">
