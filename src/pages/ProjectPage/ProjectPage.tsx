b4d1c6baeb50974d33d883745fa511480eb4afda
claratam <claramtam@gmail.com>
2023-12-22 21:27:20 -0600

    feat: fix project data import; get title from url

--- src/pages/ProjectPage/ProjectPage.tsx
+++ src/pages/ProjectPage/ProjectPage.tsx
@@ -1,30 +1,34 @@
 import './ProjectPage.scss';
-import data from '../../data/projects.json';
+import { useParams } from 'react-router';
+import { default as data } from '../../data/projects.json';
 
 
 export const ProjectPage = () => {
+    const { title } = useParams();
+    const project = data.filter(item => item.title.toLowerCase() === title)[0];
+
     return (
         <main className="project">
             <section className="project__content">
                 <article className="project__description">
-                    <h2 className="project__title">{data.title}</h2>
-                    <p className="project__summary">{data.description}</p>
+                    <h2 className="project__title">{project.title}</h2>
+                    <p className="project__summary">{project.description}</p>
 
                     <div className="project__details">
                         <span className="project__subheading">Client: </span>
-                        <p className="project__text">{data.client}</p>
+                        <p className="project__text">{project.client}</p>
                         <span className="project__subheading">Website: </span>
-                        <p className="project__text">{data.url}</p>
+                        <p className="project__text">{project.url}</p>
                         <span className="project__subheading">Date: </span>
-                        <p className="project__text">{data.date}</p>
+                        <p className="project__text">{project.date}</p>
                         <span className="project__subheading">Brief: </span>
-                        <p className="project__text">{data.brief}</p>
+                        <p className="project__text">{project.brief}</p>
                         <span className="project__subheading">Concept: </span>
-                        <p className="project__text">{data.concept}</p>
+                        <p className="project__text">{project.concept}</p>
                         <span className="project__subheading">Deliverables: </span>
-                        <p className="project__text">{data.deliverables}</p>
+                        <p className="project__text">{project.deliverables}</p>
                         <span className="project__subheading">Demographic: </span>
-                        <p className="project__text">{data.demographic}</p>
+                        <p className="project__text">{project.demographic}</p>
                     </div>
 
                 </article>
