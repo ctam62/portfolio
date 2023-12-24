1664f180e5d05bd330918e9a838202d9ab9b4403
claratam <claramtam@gmail.com>
2023-12-23 23:52:02 -0600

    refactor: add api calls to retrieve data; remove data imports from data directory; update project page navigation

--- src/pages/ProjectPage/ProjectPage.tsx
+++ src/pages/ProjectPage/ProjectPage.tsx
@@ -1,8 +1,9 @@
 import './ProjectPage.scss';
+import axios from 'axios';
 import { useParams } from 'react-router';
 import { Link, useNavigate } from 'react-router-dom';
-import data from '../../data/projects.json';
 import { useEffect, useState } from 'react';
+import github from '../../assets/icons/github.svg';
 
 
 interface props {
@@ -11,28 +12,70 @@ interface props {
 }
 
 export const ProjectPage = ({ open, setOpen }: props) => {
-    const { title } = useParams();
-    const project = data.filter(item => item.title.toLowerCase() === title?.replace(/[-]/g, " "))[0];
-    const responsiveImg = project.images.includes(`${title?.toLowerCase()}-responsive.jpg`);
-    let filename = project.images[0];
+    const apiUrl = import.meta.env.VITE_APP_API_URL;
 
-    if (responsiveImg) {
-        const targetIndex = project.images.indexOf(`${title?.toLowerCase()}-responsive.jpg`);
-        filename = project.images[targetIndex];
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [projects, setProjects] = useState([] as any[]);
+    const [project, setProject] = useState({} as any);
+
+    const { title } = useParams();
+    const parsedTitle = title?.replace(/[-]/g, " ");
+
+    useEffect(() => {
+        const getAllProjects = async () => {
+            try {
+                const { data } = await axios.get(`${apiUrl}/api/projects`);
+                setProjects(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        const getProject = async () => {
+
+            try {
+                setIsLoading(true);
+                const { data } = await axios.get(`${apiUrl}/api/projects/${parsedTitle}`);
+                setProject(data);
+                setIsLoading(false);
+            } catch (error) {
+                setIsLoading(false);
+                console.error(error);
+            }
+        }
+
+        getAllProjects();
+        if (title !== undefined) {
+            getProject();
+        }
+    }, [title]);
+
+    let responsiveImg = "";
+    let filename = "";
+    if (project.images !== undefined) {
+        responsiveImg = project.images?.includes(`${title?.toLowerCase()}-responsive.jpg`);
+        filename = `${apiUrl}/${project.images[0]}`;
+
+        if (responsiveImg) {
+            const targetIndex = project.images.indexOf(`${title?.toLowerCase()}-responsive.jpg`);
+            filename = `${apiUrl}/${project.images[targetIndex]}`;
+        }
     }
 
     const [disablePrev, setDisablePrev] = useState(false);
     const [disableNext, setDisableNext] = useState(false);
 
     const navigate = useNavigate();
-    const currentIndex = data.indexOf(project);
+    const currentIndex = project.id;
     const prevIndex = currentIndex - 1;
     const nextIndex = currentIndex + 1;
 
+
     useEffect(() => {
-        if (data.length - 1 < nextIndex) {
+        if (projects?.length < nextIndex) {
             setDisableNext(true);
-        } else if (prevIndex < 0) {
+        } else if (prevIndex === 0) {
             setDisablePrev(true);
         } else {
             setDisablePrev(false);
@@ -41,12 +84,12 @@ export const ProjectPage = ({ open, setOpen }: props) => {
     }, [prevIndex, nextIndex]);
 
     const handlePrevProject = () => {
-        const prevProjectTitle = data[prevIndex].title.toLowerCase();
+        const prevProjectTitle = projects.find(project => project.id === prevIndex).title;
         navigate(`/project/${prevProjectTitle}`);
     };
 
     const handleNextProject = () => {
-        const nextProjectTitle = data[nextIndex].title.toLowerCase();
+        const nextProjectTitle = projects.find(project => project.id === nextIndex).title;
         navigate(`/project/${nextProjectTitle}`);
     };
 
@@ -58,58 +101,69 @@ export const ProjectPage = ({ open, setOpen }: props) => {
 
     return (
         <main className="project" onClick={handleNavClose}>
-            <section className="project__content">
-                <div className="project__content-col project__content-col--first">
-                    <div
-                        className="project__imgs"
-                        style={{ background: `center/cover no-repeat url(/src/assets/images/${filename})` }}
-                    >
-                    </div>
-                </div>
-                <div className="project__content-col project__content-col--second">
-                    <article className="project__description">
-                        <h2 className="project__title">{project.title}</h2>
-                        <p className="project__summary">{project.description}</p>
-
-                        <div className="project__details">
-                            <p className={`project__text ${project.client === "" ? "hide" : ""}`}>
-                                <span className="project__subheading">Client: </span>
-                                {project.client}
-                            </p>
-
-                            <p className={`project__text ${project.url === "" ? "hide" : ""}`}>
-                                <span className="project__subheading">Website: </span>
-                                <Link to={project.url} target="_blank">{project.url.replace(/(https:\/\/)|(http:\/\/)/g, "")}</Link>
-                            </p>
-
-                            <p className={`project__text ${project.date === "" ? "hide" : ""}`}>
-                                <span className="project__subheading">Date: </span>
-                                {project.date}
-                            </p>
-
-                            <p className={`project__text ${project.brief === "" ? "hide" : ""}`}>
-                                <span className="project__subheading">Brief: </span>
-                                {project.brief}
-                            </p>
-
-                            <p className={`project__text ${project.concept === "" ? "hide" : ""}`}>
-                                <span className="project__subheading">Concept: </span>
-                                {project.concept}
-                            </p>
-
-                            <p className={`project__text ${project.deliverables === "" ? "hide" : ""}`}>
-                                <span className="project__subheading">Deliverables: </span>
-                                {project.deliverables}
-                            </p>
-
-                            <p className={`project__text ${project.demographic === "" ? "hide" : ""}`}>
-                                <span className="project__subheading">Demographic: </span>
-                                {project.demographic}
-                            </p>
+            {isLoading ? <p>Loading...</p> : null}
+            {project &&
+                <section className="project__content">
+                    <div className="project__content-col project__content-col--first">
+                        <div
+                            className="project__imgs"
+                            style={{ background: `center/cover no-repeat url(${filename})` }}
+                        >
                         </div>
-                    </article>
-                </div>
-            </section>
+                    </div>
+                    <div className="project__content-col project__content-col--second">
+                        <article className="project__description">
+                            <h2 className="project__title">{project.title}</h2>
+                            <p className="project__summary">{project.description}</p>
+
+                            <div className="project__details">
+                                <p className={`project__text ${project.client === "" ? "hide" : ""}`}>
+                                    <span className="project__subheading">Client: </span>
+                                    {project.client}
+                                </p>
+                                <p className="project__text">
+                                    <span className="project__subheading">Date: </span>
+                                    {project.date}
+                                </p>
+                                <p className={`project__text ${project.brief === "" ? "hide" : ""}`}>
+                                    <span className="project__subheading">Brief: </span>
+                                    {project.brief}
+                                </p>
+                                <p className={`project__text ${project.concept === "" ? "hide" : ""}`}>
+                                    <span className="project__subheading">Concept: </span>
+                                    {project.concept}
+                                </p>
+                                <p className={`project__text ${project.deliverables === "" ? "hide" : ""}`}>
+                                    <span className="project__subheading">Deliverables: </span>
+                                    {project.deliverables}
+                                </p>
+                                <p className={`project__text ${project.demographic === "" ? "hide" : ""}`}>
+                                    <span className="project__subheading">Demographic: </span>
+                                    {project.demographic}
+                                </p>
+
+                                <p className="project__text">
+                                    <span className="project__subheading">Tech Stack: </span>
+                                    {project.tech}
+                                </p>
+
+                                <p className={`project__text ${project?.url === "" ? "hide" : ""}`}>
+                                    <span className="project__subheading">Website: </span>
+                                    <Link to={project.url} target="_blank">
+                                        {project.url?.replace(/(https:\/\/)|(http:\/\/)/g, "")}
+                                    </Link>
+                                </p>
+                                <p className={`project__text ${project.github === "" ? "hide" : ""}`}>
+                                    <Link to={project.github} target="_blank">
+                                        <img className="project__icon" src={github} alt="github" />
+                                    </Link>
+                                </p>
+                            </div>
+                        </article>
+                    </div>
+                </section>
+            }
+
             <nav className="project__nav">
                 <ul className="project__nav-list">
                     <li
