e7db2d0b3d8d2e6a7fb3d4699e3979421074fdc5
claratam <claramtam@gmail.com>
2024-01-03 12:24:16 -0600

    feat: hide skills section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Hero } from '../../components/Hero/Hero';
 import { About } from '../../components/About/About';
-import { Skills } from '../../components/Skills/Skills';
+// import { Skills } from '../../components/Skills/Skills';
 import { Projects } from '../../components/Projects/Projects';
 import { Contact } from '../../components/Contact/Contact';
 import upArrow from '../../assets/icons/arrow-up-circle.svg';
